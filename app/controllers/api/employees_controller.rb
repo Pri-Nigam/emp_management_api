6ca5af960473bc364781e5b8464cf7a999e3bd15
Pri-Nigam <prinigam@bestpeers.com>
2023-07-06 16:27:38 +0530

    Added create,update and show action

--- app/controllers/api/employees_controller.rb
+++ app/controllers/api/employees_controller.rb
@@ -3,4 +3,27 @@ class Api::EmployeesController < ApplicationController
     @employees = Employee.all
     render json: { employees: @employees }
   end
+
+  def show
+    employee = Employee.find(params[:id])
+    render json: employee
+  end
+
+  def create
+    employee = Employee.new(employee_params)
+    employee.save
+  end
+
+  def update
+    employee = Employee.find(params[:id])
+    employee.update(employee_params)
+    employee.save
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :email, :password, :date_of_birth, :gender, :contact, :designation)
+  end
+
 end
